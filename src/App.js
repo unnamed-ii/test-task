9f7448dc0962788bba75e73d85073df3613b3ffc
unnamed-ii <isaingashev@gmail.com>
2023-01-23 22:29:54 +0300

    added styles

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ function App() {
                 <div className="app__inner-list">
                     {ProductsData.map(product => (
                         <Card
+                            key={Math.floor(Math.random()*10000)}
                             title={product.title}
                             amount={product.amount}
                             description={product.description}
--- src/components/Card/card.jsx
+++ src/components/Card/card.jsx
@@ -1,22 +1,28 @@
 import React from 'react';
+import './style.scss';
+import cat from '../../images/product-cat.png'
 
 export const Card = ({title, amount, description, weight, isAvailable}) => {
 
     return (
         <div className={"card " + (isAvailable && "available")}>
             <div className="card__main">
-                <div className="card__main-question">
+                <div className="card__main-subtitle">
                     Котэ не одобряет?
                 </div>
                 <div className="card__main-title">
-                    Нямушка с {title}
+                    Нямушка
+                    <div className="card__main-title__filling">
+                        с {title}
+                    </div>
                 </div>
                 <div className="card__main-amount">
                     {amount}
                 </div>
                 <div className="card__main-weight">
-                    {weight} КГ
+                    {weight} <span>КГ</span>
                 </div>
+                <img src={cat} alt="Image"/>
             </div>
             <div className="card__description">
                 {description}
